6ba8b8fc2f69b7066b42da0e48caf6e205505434
kaioman <kajin0318@gmail.com>
2026-01-24 23:57:11 +0900

    カスタムノードimage_edit追加

--- comfyui/ComfyUI/custom_nodes/image_edit.py
+++ comfyui/ComfyUI/custom_nodes/image_edit.py
@@ -1,8 +1,10 @@
 import os
-import tempfile
 import subprocess
 import numpy as np
+import libcore_hng.utils.app_logger as app_logger
+import pycorex.configs.app_init as app
 from PIL import Image
+from pathlib import Path
 
 class ImageEdit:
     
@@ -20,26 +22,54 @@ class ImageEdit:
     CATEGORY = "Image/Editing"
     
     def run(self, prompt, input_image):
-        print("ImageEdit node called", flush=True)
-        print(prompt, flush=True)
 
+        # pycorex初期化
+        app.init_app(__file__, "logger.json", "pycorex.json")
+        app_logger.info("ImageEdit node called")
+        
         # Tensor → PIL Imageに変換
         image_np = (input_image[0].cpu().numpy() * 255).astype(np.uint8)
         image_pil = Image.fromarray(image_np)
         
-        # 一時ファイルに保存
-        with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmp_file:
-            image_path = tmp_file.name
-            image_pil.save(image_path)
-            
-        # プロンプトを外部スクリプトに渡して実行
-        script_path = os.path.join(os.path.dirname(__file__), "..", "utils", "image_edit_output.py")
-        subprocess.run([
-            "python",
-            script_path,
-            "--prompt", prompt,
-            "--input", image_path,
-        ], check=True)
+        # ルート取得
+        base_dir = os.getenv("PROJECT_ROOT")
+        if not base_dir:
+            raise RuntimeError("PROJECT_ROOT environment variable is not set")
+
+        # 出力先
+        save_dir = Path(base_dir) / "source_image"
+        save_dir.mkdir(parents=True, exist_ok=True)        
+        # ファイル名
+        import uuid
+        filename = f"{uuid.uuid4().hex}.png"
+        app_logger.info(f"Generated filename: {filename}")
+
+        # 元画像保存
+        image_path = save_dir / filename
+        image_pil.save(image_path)
+        app_logger.info(f"Input image saved to: {image_path}")
+
+        try:
+            # プロンプトを外部スクリプトに渡して実行
+            script_path = os.path.join(os.path.dirname(__file__), "..", "utils", "image_edit_output.py")
+            app_logger.info("Calling external image edit script...")
+            subprocess.run([
+                "python",
+                script_path,
+                "--prompt", prompt,
+                "--input", image_path,
+            ], check=True)
+        except subprocess.CalledProcessError as e:        
+            app_logger.error(f"External script failed: {e}")
+        finally:
+            try:
+                # 画像削除
+                if image_path.exists():
+                    image_path.unlink()
+                    app_logger.info(f"Temporary image file deleted: {image_path}")
+
+            except Exception as e:
+                app_logger.error(f"Failed to delete temporary image file: {e}")
         
         # ダミー出力
         return (input_image,)
--- comfyui/ComfyUI/utils/image_edit_output.py
+++ comfyui/ComfyUI/utils/image_edit_output.py
@@ -1,4 +1,9 @@
 import argparse
+import libcore_hng.utils.app_logger as app_logger
+import pycorex.configs.app_init as app
+from pycorex.gemini_client import GeminiClient
+from pycorex.uwgen_client import UwgenClient
+from pycorex.exceptions.no_candidates_error import NoCandidatesError
 
 def parse_args():
     parser = argparse.ArgumentParser(description="External image edit script")
@@ -9,11 +14,43 @@ def parse_args():
 def main():
     args = parse_args()
 
+    # 引数取得
     prompt = args.prompt
     input_path = args.input
+    
+    # pycorex初期化
+    app.init_app(__file__, "logger.json", "pycorex.json")
 
-    print(f"prompt:{prompt}")
-    print(f"input_path:{input_path}")
+    # プロジェクトルート
+    app_logger.info(f"Project root:{app.core.config.project_root_path}")
+    
+    # UwgenClient初期化
+    client = UwgenClient()
+    app_logger.info(f"Uwgen endpoint:{client.endpoint}")
+    
+    # 元画像取得
+    source_file_path = input_path
+    app_logger.info(f"Source file path:{source_file_path}")
+    
+    try:
+        # 画像生成を実行
+        result = client.edit_image(
+            prompt=prompt,
+            source_image_path=source_file_path,
+            model=GeminiClient.GeminiModel.GEMINI_3_0_PRO_IMAGE_PREVIEW.value,
+            resolution=GeminiClient.ImageSize.TWO_K.value,
+            aspect=GeminiClient.AspectRatio.SQUARE.value,
+            safety_filter = GeminiClient.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT.value,
+            safety_level = GeminiClient.SafetyFilterLevel.BLOCK_ONLY_HIGH.value
+        )
+        
+        # 画像ファイルを出力する
+        client.output_images(result["images"], "gen_images")
+
+    except NoCandidatesError as e:
+        app_logger.error(f"Image generation failed: {e}")
+    except Exception as e:
+        app_logger.error(f"Unexpected error: {e}")
     
 if __name__ == "__main__":
     main()
